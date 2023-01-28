d81794595b6f36f975f7af0f83dbd13b99c802ec
Rohan Arora <mac@macs-MacBook-Air.local>
2023-01-28 12:20:32 +0530

    Downloaded spring boot maven app from start.spring.io

---
+++ src/main/java/com/luv2code/springboot/demo/startApp/StartAppApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.demo.startApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartAppApplication.class, args);
+	}
+
+}
