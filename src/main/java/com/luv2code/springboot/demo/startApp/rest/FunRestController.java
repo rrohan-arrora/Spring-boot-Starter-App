a961a18820fc2ff1a134e350dfbbcebeefe05f63
Rohan Arora <mac@macs-MacBook-Air.local>
2023-01-28 15:00:43 +0530

    Added spring-dev-tools to auto represent the changes

--- src/main/java/com/luv2code/springboot/demo/startApp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/startApp/rest/FunRestController.java
@@ -8,6 +8,17 @@ public class FunRestController {
 	
 	@GetMapping("/")
 	public String sayHello() {
-		return "Hello Mother fuckers " + "and bitches";
+		return "Hello Mother fuckers " + "and whores. Pitty you!";
+	}
+	
+	// expose a new endpoint for "workout"
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "GYM chala jaa bsdk";
+	}
+	
+	@GetMapping("/anoushka")
+	public String thePast() {
+		return "bahut pachtaegi tu!";
 	}
 }
