76c5e19bd1abf6bd6ddbbfb70a0645c37c09ae01
Rohan Arora <mac@macs-MacBook-Air.local>
2023-01-30 11:27:02 +0530

    Filled values using properties file

--- src/main/java/com/luv2code/springboot/demo/startApp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/startApp/rest/FunRestController.java
@@ -1,10 +1,20 @@
 package com.luv2code.springboot.demo.startApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	// defining the properties for which the values are filled
+	// using properties file.
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String coachTeam;
+	
 	
 	@GetMapping("/")
 	public String sayHello() {
@@ -21,4 +31,10 @@ public class FunRestController {
 	public String thePast() {
 		return "bahut pachtaegi tu!";
 	}
+	
+	@GetMapping("/properties")
+	public String showProperties() {
+		return ""+coachName + " " + coachTeam;
+	}
+	
 }
